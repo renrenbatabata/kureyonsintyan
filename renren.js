79a294e098c013ee48f97bd4068e217f2f97356e
renren <148751281+renrenbatabata@users.noreply.github.com>
2023-11-04 17:46:38 +0900

    ケツだし星人に横移動のアニメーションを追加しました。

--- renren.js
+++ renren.js
@@ -1,10 +1,12 @@
-const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+const dazos = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-headings.forEach((heading) => {
-  heading.textContent += 'だゾ';
+dazos.forEach((dazo) => {
+  dazo.textContent += 'だゾ';
 });
 
 
+
+const headings = document.querySelectorAll('h1, h2, h3');
 // 画像を作成
 const gifUrl = chrome.runtime.getURL("./ketu.gif");
 
@@ -12,6 +14,7 @@ const gifUrl = chrome.runtime.getURL("./ketu.gif");
 headings.forEach((heading) => {
   // 画像要素を作成
   const img = document.createElement('img');
+  img.classList.add("ketu");
   img.src = gifUrl;
   img.alt = 'ケツだし星人';
 
