b22c525463a7dea60e55f3c29056fe2ec6f0411a
renren <148751281+renrenbatabata@users.noreply.github.com>
2023-11-04 16:50:33 +0900

    ケツだし星人画像追加できました！

--- renren.js
+++ renren.js
@@ -3,3 +3,24 @@ const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 headings.forEach((heading) => {
   heading.textContent += 'だゾ';
 });
+
+
+// 画像を作成
+const gifUrl = chrome.runtime.getURL("./ketu.gif");
+
+// 各見出し要素の上にGIF画像を追加
+headings.forEach((heading) => {
+  // 画像要素を作成
+  const img = document.createElement('img');
+  img.src = gifUrl;
+  img.alt = 'ケツだし星人';
+
+  // 見出し要素をラップするコンテナを作成
+  const container = document.createElement('div');
+  container.style.position = 'relative';
+  container.appendChild(heading.cloneNode(true)); // オリジナルの見出しをコピーしてコンテナに追加
+  container.appendChild(img); // GIF画像をコンテナに追加
+
+  // オリジナルの見出しを置き換える
+  heading.parentNode.replaceChild(container, heading);
+});
