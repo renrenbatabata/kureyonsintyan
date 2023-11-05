be3ff23308c133aaa6cf7fe6538038852c6280bc
renren <148751281+renrenbatabata@users.noreply.github.com>
2023-11-05 14:08:34 +0900

    再生されているのがあったら再生されないように修正しました。

--- haru.js
+++ haru.js
@@ -1,13 +1,19 @@
+let audio = null; // Audio要素を外部に持つ
+
 const titles = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
 
 titles.forEach(title => {
   title.addEventListener("mouseover", (event) => {
+    if (!audio || audio.paused) {
+      const music = chrome.runtime.getURL("title_call.mp3");
+      audio = new Audio(music);
+      audio.autoplay = true;
+      audio.play();
+    }
+
     event.target.style.color = "orange";
-    const music = chrome.runtime.getURL("title_call.mp3");
-    let audio = new Audio(music);
-    audio.autoplay = true;
-    audio.play();
   });
+
   title.addEventListener("mouseout", (event) => {
     event.target.style.color = "";
   });
