8313159e01c2793af55641f00eb9654eac75363a
haru <hinagoro5656@gmail.com>
2023-11-05 11:53:31 +0900

    ホバーしている間だけ色を変えるようにした

--- haru.js
+++ haru.js
@@ -3,11 +3,12 @@ const titles = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
 titles.forEach(title => {
   title.addEventListener("mouseover", (event) => {
     event.target.style.color = "orange";
-    setTimeout(() => {
-      event.target.style.color = "";
-    }, 1000);
     const music = chrome.runtime.getURL("title_call.mp3");
     let audio = new Audio(music);
+    audio.autoplay = true;
     audio.play();
+  });
+  title.addEventListener("mouseout", (event) => {
+    event.target.style.color = "";
   });
 });
