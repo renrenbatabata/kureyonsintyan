eb8d15ec4660c51daf503366ce87c60550e90a10
renren <148751281+renrenbatabata@users.noreply.github.com>
2023-11-05 14:14:53 +0900

    試しに入れてみた跳ねる動作を削除しました

--- massan.js
+++ massan.js
@@ -3,6 +3,6 @@ for (let i = 0; i < images.length; i++) {
     if(images[i].src != chrome.runtime.getURL('ketu.gif')){
         images[i].src = chrome.runtime.getURL('ketujin.png');
     }
-    
+
 }
 
